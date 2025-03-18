8bc5202cc193b3eb91a9cbba547190e023240615
Dylan Stark <dylan.stark@gmail.com>
2025-03-18 16:00:09 -0500

    Adds Cellular Automata.

--- .config/nvim/lua/dylan-stark/remap.lua
+++ .config/nvim/lua/dylan-stark/remap.lua
@@ -8,3 +8,6 @@ vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>")
 vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>")
 
+-- Celluar Automata
+vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>")
