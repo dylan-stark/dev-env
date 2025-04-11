0cb8a56dcb9459c67afe656b351dfccd6b83201f
Dylan Stark <dylan.stark@gmail.com>
2025-04-11 05:36:06 -0500

    Updates maps.

--- .config/nvim/lua/dylan-stark/init.lua
+++ .config/nvim/lua/dylan-stark/init.lua
@@ -2,6 +2,12 @@ require("dylan-stark.lazy")
 require("dylan-stark.remap")
 require("dylan-stark.rust")
 
+-- Setting spaces instead of tabs
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
 -- Relative line numbering
 vim.wo.relativenumber = true
 vim.wo.number = true
--- .config/nvim/lua/dylan-stark/lazy.lua
+++ .config/nvim/lua/dylan-stark/lazy.lua
@@ -20,11 +20,6 @@ vim.opt.rtp:prepend(lazypath)
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
--- Setting spaces instead of tabs
-vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
-vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
-vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
 
 -- Setup lazy.nvim
 require("lazy").setup({
