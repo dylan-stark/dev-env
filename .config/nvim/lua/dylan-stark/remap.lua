50d2e98b12006cf4db84cc41d55a0d4e414b959c
Dylan Stark <dylan.stark@gmail.com>
2025-04-16 03:41:09 -0500

    Adds cool visual mode maps.

--- .config/nvim/lua/dylan-stark/remap.lua
+++ .config/nvim/lua/dylan-stark/remap.lua
@@ -1,6 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Source current buffer
+vim.keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+
 -- Kick off tmux-sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>")
 
@@ -16,4 +21,8 @@ vim.keymap.set("n", "<leader>gol", "<cmd>CellularAutomaton game_of_life<CR>")
 vim.keymap.set("v", "<leader>y", '"*y') -- yank this
 vim.keymap.set("n", "<leader>y", '"*y') -- prepare to yank
 vim.keymap.set("n", "<leader>Y", 'gg"*yG') -- yank whole file
+
+-- Move visual blocks up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
