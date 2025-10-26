13aa9d2eb306be27cf9a002a47d2ed8da9294307
Dylan Stark <dylan.stark@gmail.com>
2025-10-26 07:30:52 -0500

    Adds tmux switcher keymap.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Kick off tmux-sessionizer
+vim.keymap.del("n", "<C-f>")
+vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>")
