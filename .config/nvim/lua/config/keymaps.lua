4ff34effa015a62dcd5b75c79ce8774d43e846fb
Dylan Stark <dylan.stark@gmail.com>
2025-10-26 07:37:12 -0500

    Adds yank-to-clipboard keymap.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,6 +2,11 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- Yank to clipboard
+vim.keymap.set("v", "<leader>yy", '"*y') -- yank this
+vim.keymap.set("n", "<leader>yy", '"*y') -- prepare to yank
+vim.keymap.set("n", "<leader>YY", 'gg"*yG') -- yank whole file
+
 -- Kick off tmux-sessionizer
 vim.keymap.del("n", "<C-f>")
 vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>")
